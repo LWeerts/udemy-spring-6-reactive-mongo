a975e37de15dd6b602a6017bfac4a9f92e2c262b
LWeerts <83358839+LWeerts@users.noreply.github.com>
2025-08-25 17:29:37 +0200

    Implemented CustomerServiceImpl with endpoints and tests

---
+++ src/test/java/guru/springframework/reactivemongo/web/fn/CustomerEndpointTest.java
@@ -0,0 +1,199 @@
+package guru.springframework.reactivemongo.web.fn;
+
+import guru.springframework.reactivemongo.domain.Customer;
+import guru.springframework.reactivemongo.mappers.CustomerMapper;
+import guru.springframework.reactivemongo.model.CustomerDTO;
+import guru.springframework.reactivemongo.services.CustomerService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class CustomerEndpointTest {
+
+    @Container
+    @ServiceConnection
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    CustomerMapper customerMapper;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void testPatchIdNotFound() {
+        webTestClient.patch()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, -1)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testPatchIdFound() {
+        CustomerDTO customerDTO = getSavedTestCustomer();
+
+        webTestClient.patch()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .body(Mono.just(customerDTO), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    void testDeleteNotFound() {
+        webTestClient.delete()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, -1)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(999)
+    void testDeleteCustomer() {
+        CustomerDTO customerDTO = getSavedTestCustomer();
+
+        webTestClient.delete()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateCustomerBadRequest() {
+        CustomerDTO savedTestCustomer = getSavedTestCustomer();
+        savedTestCustomer.setCustomerName("");
+
+        webTestClient.put()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, savedTestCustomer.getId())
+                .body(Mono.just(savedTestCustomer), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void testUpdateCustomerNotFound() {
+        webTestClient.put()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, -1)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(3)
+    void testUpdateCustomer() {
+        CustomerDTO customerDTO = getSavedTestCustomer();
+        customerDTO.setCustomerName("Jane");
+
+        webTestClient.put()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .body(Mono.just(customerDTO), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    void testCreateCustomerBadData() {
+        CustomerDTO customerDTO = getSavedTestCustomer();
+        customerDTO.setCustomerName("");
+
+        webTestClient.post().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .body(Mono.just(customerDTO), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void testCreateCustomer() {
+        CustomerDTO customerDTO = getSavedTestCustomer();
+
+        webTestClient.post().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .body(Mono.just(customerDTO), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().exists("location");
+    }
+
+    @Test
+    void testGetByIdNotFound() {
+        webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH_ID, -1)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(1)
+    void testGetById() {
+        CustomerDTO savedTestCustomer = getSavedTestCustomer();
+
+        webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH_ID, savedTestCustomer.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(CustomerDTO.class);
+    }
+
+    @Test
+    @Order(2)
+    void testListCustomers() {
+        webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()").value(greaterThan(0));
+    }
+
+
+//
+//    public CustomerDTO getSavedCustomerDto() {
+//        return customerService.saveCustomer(
+//                Mono.just(customerMapper.customerToCustomerDto(getTestCustomer()))
+//        ).block();
+//    }
+
+    public CustomerDTO getSavedTestCustomer() {
+        FluxExchangeResult<CustomerDTO> customerDTOFluxExchangeResult = webTestClient.post()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .returnResult(CustomerDTO.class);
+
+        List<String> location = customerDTOFluxExchangeResult.getResponseHeaders().get("Location");
+
+        return webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .exchange().returnResult(CustomerDTO.class).getResponseBody().blockFirst();
+    }
+
+    public static Customer getTestCustomer() {
+        return Customer.builder()
+                .customerName("John")
+                .build();
+    }
+}
